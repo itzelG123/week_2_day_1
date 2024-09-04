62beed1cb6ee3e56795c39133d5aa47b499a81b5
Garza <igarza8@cps.k12.il.us>
2024-09-04 15:08:12 -0500

    string slicing

--- week2.py
+++ week2.py
@@ -7,3 +7,6 @@ print("Yours favorite holiday is " + holiday)
 #print out the day and their fav hoilday in one sentence 
 print("Today is"+ day + "and your favorite holiday is" + holiday)
 #make change to file
+#string slicing
+word="hello"
+print(word[1:4])
